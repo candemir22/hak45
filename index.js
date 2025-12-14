e37e53e143cc0311dee080edfd23b1381c815951
xarita <128380509+candemir22@users.noreply.github.com>
2025-12-14 07:24:57 +0400

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ let map;
 async function initMap() {
   // Request the "maps" library dynamically
   const { Map } = (await google.maps.importLibrary('maps'));
-
+  const { AdvancedMarkerElement } = (await google.maps.importLibrary('marker'));
   // The location of a specific place (e.g., Uluru, Australia)
   const position = { lat: 42.344, lng: 42.031 };
 
   // Create a map centered at the specified position
   map = new Map(document.getElementById('map'), {
-    zoom: 15,
+    zoom: 8,
     center: position,
     mapId: '8b37d7206ccf0121d4414bb0',     // Using a demo ID for illustration
   });
