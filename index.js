efe6cb7b350dca591a60e2460395b9ad9a2733ac
xarita <128380509+candemir22@users.noreply.github.com>
2025-12-14 07:35:40 +0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function initMap() {
   // Request the "maps" library dynamically
   const { Map } = (await google.maps.importLibrary('maps'));
   const { AdvancedMarkerElement } = (await google.maps.importLibrary('marker'));
-    gmp-map center="42.773,42.01" zoom="28" ;
+ 
   // The location of a specific place (e.g., Uluru, Australia)
   const position = { lat: 42.344, lng: 43.031 };
 
